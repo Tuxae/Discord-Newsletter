6f06088785d6bb4fb1f6ea9faf5c4601f39bcca1
Adrien MEILAC <33166122+Adrien-Meilac@users.noreply.github.com>
2020-03-11 23:14:39 +0100

    hide unicode char

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('Bot is login as ({0.user.name}, {0.user.id})'.format(client))
+    print('Bot is logging as ({0.user.name}, {0.user.id})'.format(client))
     
 @client.event
 async def look_for_email_news():
@@ -20,7 +20,7 @@ async def look_for_email_news():
       mailbox = MailBox(mail_server)
       mailbox.login(mail_address, mail_password, initial_folder = mail_folder)
       for msg in mailbox.fetch(Q(seen=False)):
-         if "[🐍PyTricks]" in msg.subject:
+         if "[\U0001f40dPyTricks]" in msg.subject:
             print("A new message was found !")
             txt = msg.text[:msg.text.find("This email is part of the Python")]           
             await client.get_channel(channel_id).send(txt)
